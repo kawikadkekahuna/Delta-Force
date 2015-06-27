2c28d60a904a31a3380ef8129f2d810a20c3e433
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-26 20:04:49 -1000

    added stage 2

--- js/Timer.js
+++ js/Timer.js
@@ -1,22 +1,67 @@
 var EventEmitter = require('events');
 var util = require('util');
+// var timers = require('timers');
 
-function Timer () {
+function Timer() {
   EventEmitter.call(this);
   var self = this;
-  this.i = 0;
-  setInterval(function () {
-    self.emit('tick', { interval : self.i++ });
-  }, 1000);
+  this.startTime;
+  this.endTime;
+  var i = 0;
+
+
+  this.start = function() {
+
+    self.emit('start', {
+      startMS: Date.now()
+    })
+
+    setInterval(function() {
+      self.emit('pass', {
+        interval: i++
+      })
+    }, 1000);
+
+  }
+
+  this.stop = function() {
+    self.emit('stop', {
+      stopMS: Date.now()
+    });
+
+  }
 }
 
 util.inherits(Timer, EventEmitter);
 
 
-function tickHandler(event){
-  process.stdout.write('tick ' + this.i + '\n');
+
+var testTime = new Timer();
+
+function tickCounter(event) {
+  process.stdout.write('index ' + event.interval + '\n');
+  if (event.interval === 12) {
+    testTime.removeListener('pass', tickCounter);
+    testTime.stop();
+  }
 }
 
+function setStartTime(event) {
+  this.startTime = event.startMS;
+  // process.stdout.write('startTime' + this.startTime + '\n');
+}
 
-var myTimer = new Timer();
-myTimer.addListener('tick', tickHandler);
+function setStopTime(event) {
+  this.endTime = event.stopMS;
+  var elapsed = this.startTime - this.endTime;
+  // process.stdout.write('stopTime' + this.endTime + '\n');
+  process.stdout.write('time passed: ' + elapsed + ' ms');
+}
+
+testTime.addListener('start', setStartTime);
+
+testTime.addListener('stop', setStopTime);
+
+testTime.addListener('pass', tickCounter);
+
+testTime.start();
