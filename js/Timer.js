13c9be2a6e172dac6f9a98990946aac281549570
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-26 18:32:23 -1000

    ticks every second

--- js/Timer.js
+++ js/Timer.js
@@ -1,12 +1,22 @@
+var EventEmitter = require('events');
+var util = require('util');
 
-
-
-
-
-
-function Timer(){
-
-
-
-
+function Timer () {
+  EventEmitter.call(this);
+  var self = this;
+  this.i = 0;
+  setInterval(function () {
+    self.emit('tick', { interval : self.i++ });
+  }, 1000);
 }
+
+util.inherits(Timer, EventEmitter);
+
+
+function tickHandler(event){
+  process.stdout.write('tick ' + this.i + '\n');
+}
+
+
+var myTimer = new Timer();
+myTimer.addListener('tick', tickHandler);
