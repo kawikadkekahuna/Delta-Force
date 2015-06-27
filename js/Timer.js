bf62d6f40be1688a248d7bb3df535359985cc7fd
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-06-27 10:02:41 -1000

    added timer that accepts an arguement to count ticks

--- js/Timer.js
+++ js/Timer.js
@@ -2,7 +2,7 @@ var EventEmitter = require('events');
 var util = require('util');
 // var timers = require('timers');
 
-function Timer() {
+function Timer(duration) {
   EventEmitter.call(this);
   var self = this;
   this.startTime;
@@ -18,7 +18,7 @@ function Timer() {
   this.start = function() {
     if (!this.getHeartbeat()) {
       heartbeat = true;
-      this.intervalID = setInterval(this.startInterval, 205);
+      this.intervalID = setInterval(this.startInterval, 100);
       self.emit('start', {
         startMS: Date.now()
       });
@@ -43,39 +43,39 @@ function Timer() {
   }
 
 
+  function tickCounter(event) {
+    process.stdout.write('index ' + event.interval + '\n');
+    if (event.interval === duration) {
+      testTime.stop();
+
+    }
+  }
+
+  function setStartTime(event) {
+    this.startTime = event.startMS;
+    process.stdout.write('start time: ' + this.startTime + '\n');
+    process.stdout.write('interval on: ' + this.getHeartbeat() + '\n');
+  }
+
+  function setStopTime(event) {
+    this.endTime = event.stopMS;
+    var elapsed = this.startTime - this.endTime;
+    process.stdout.write('start time: ' + this.startTime + '\n');
+    process.stdout.write('interval on: ' + this.getHeartbeat() + '\n');
+    process.stdout.write('time passed: ' + elapsed + ' ms');
+  }
+
+  this.addListener('start', setStartTime);
+
+  this.addListener('stop', setStopTime);
+
+  this.addListener('pass', tickCounter);
+
+
 
 }
 
 util.inherits(Timer, EventEmitter);
 
-var testTime = new Timer();
-
-function tickCounter(event) {
-  process.stdout.write('index ' + event.interval + '\n');
-  if (event.interval === 12) {
-    testTime.stop();
-
-  }
-}
-
-function setStartTime(event) {
-  this.startTime = event.startMS;
-  process.stdout.write('start time: ' + this.startTime + '\n');
-  process.stdout.write('interval on: ' + this.getHeartbeat() + '\n');
-}
-
-function setStopTime(event) {
-  this.endTime = event.stopMS;
-  var elapsed = this.startTime - this.endTime;
-  process.stdout.write('start time: ' + this.startTime + '\n');
-  process.stdout.write('interval on: ' + this.getHeartbeat() + '\n');
-  process.stdout.write('time passed: ' + elapsed + ' ms');
-}
-
-testTime.addListener('start', setStartTime);
-
-testTime.addListener('stop', setStopTime);
-
-testTime.addListener('pass', tickCounter);
-
+var testTime = new Timer(30);
 testTime.start();
